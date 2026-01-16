a8687489f63250d6b467e8de2a6af04515309533
Vivek Raj Gupta <nehavivekshah@gmail.com>
2026-01-16 07:53:46 +0530

    Specify database table for Food model

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -5,6 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Food extends Model
 {
+    
+    protected $table = 'food';
+    
     public function cartItems()
     {
         return $this->hasMany(CartItem::class);
